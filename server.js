8122aaa63d0028ed8764c3f49b47328d02eb9e68
Eddie Yao <adpoyao@gmail.com>
2017-10-11 13:44:49 -0700

    implemented get and post

--- server.js
+++ server.js
@@ -3,23 +3,41 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
-const {BlogPosts} = require('./models');
+const {BlogPosts} = require('./model');
 const bodyParser = require('body-parser');
-app.use(morgan('common'));
 const jsonParser = bodyParser.json();
 
-app.get('./blog-post', () => {
+app.use(morgan('common'));
+
+BlogPosts.create('Cats', 'Space Cats', 'Eddie', '10-11-2017');
+
+app.get('/blog-post', (req, res) => {
+  res.json(BlogPosts.get());
+});
+
+app.post('/blog-post', jsonParser, (req, res) => {
+  const requiredField = ['title', 'content', 'author', 'publishDate'];
+  for(let i=0; i<requiredField.length; i++){
+    const field = requiredField[i];
+    if(!(field in req.body)){
+      const message = `Cannot post blog due to missing ${field}`;
+      console.error(message);
+      return res.status(400).send(message);
+    } 
+  }
+  const {title, content, author, publishDate} = req.body;
+  const newPost = BlogPosts.create(title, content, author, publishDate);
+  res.json(newPost);
+});
+
+app.delete('/blogpost/:id', () => {
 
 });
 
-app.post('./blog-post', () => {
+app.put('/blogpost/:id', () => {
 
 });
 
-app.delete('./blogpost/:id', () => {
-
-});
-
-app.put('.blogpost/:id', () => {
-
+app.listen(8080, ()=> {
+  console.log('Your app is listening on 8080');
 });
