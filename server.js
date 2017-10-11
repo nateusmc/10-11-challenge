46fb05041547aaac9e6a3d30ddf80fe9fb192ad2
Nate Garrett <nate.garrett1992@gmail.com>
2017-10-11 16:29:40 -0500

    ready to merge with master - still missing router

--- server.js
+++ server.js
@@ -30,12 +30,38 @@ app.post('/blog-post', jsonParser, (req, res) => {
   res.json(newPost);
 });
 
-app.delete('/blogpost/:id', () => {
-
+app.delete('/blog-post/:id', (req, res) => {
+  BlogPosts.delete(req.params.id);
+  res.status(204).end();
 });
 
-app.put('/blogpost/:id', () => {
-
+app.put('/blog-post/:id', jsonParser, (req, res) => {
+  const requiredFields = [
+    'id', 'title', 'content', 'author', 'publishDate'];
+  for (let i=0; i<requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body`;
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+  if (req.params.id !== req.body.id) {
+    const message = (
+      `Request path id (${req.params.id}) and request body id `
+      + `(${req.body.id}) must match`);
+    console.error(message);
+    return res.status(400).send(message);
+  }
+  console.log(`Updating blog post with id \`${req.params.id}\``);
+  const updatedItem = BlogPosts.update({
+    id: req.params.id,
+    title: req.body.title,
+    content: req.body.content,
+    author: req.body.author,
+    publishDate: req.body.publishDate
+  });
+  res.status(204).end();
 });
 
 app.listen(8080, ()=> {
