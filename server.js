21ff9cb7e05b2aa625138654f223444805a4d9f6
Nate Garrett <nate.garrett1992@gmail.com>
2017-10-11 14:32:55 -0500

    setup control flow - swaping to navigator

--- server.js
+++ server.js
@@ -1,1 +1,25 @@
 'use strict';
+
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const {BlogPosts} = require('./models');
+const bodyParser = require('body-parser');
+app.use(morgan('common'));
+const jsonParser = bodyParser.json();
+
+app.get('./blog-post', () => {
+
+});
+
+app.post('./blog-post', () => {
+
+});
+
+app.delete('./blogpost/:id', () => {
+
+});
+
+app.put('.blogpost/:id', () => {
+
+});
